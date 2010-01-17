f34df90dff528e56006b9b9e365702bf313c2dc1
remi <remi@remitaylor.com>
2010-01-17 03:51:26 -0700

    Deck.new gives you a standard 52 card deck

--- lib/playing-cards.rb
+++ lib/playing-cards.rb
@@ -1,6 +1,11 @@
+require 'forwardable'
+
 # Misc playing card stuff
 
 class Card
+
+  STANDARD_PLAYING_CARD_NAMES  = %w( 2 3 4 5 6 7 8 9 10 Jack Queen King Ace )
+  STANDARD_PLAYING_CARD_SUITES = %w( Spades Hearts Clubs Diamonds )
 
   attr_accessor :name, :suite
 
@@ -28,6 +33,18 @@ class Card
     Card.new name, suite
   end
 
+  def self.names
+    STANDARD_PLAYING_CARD_NAMES
+  end
+
+  def self.suites
+    STANDARD_PLAYING_CARD_SUITES
+  end
+
+  def == another_card
+    another_card.name == name && another_card.suite == suite if another_card.is_a?(Card)
+  end
+
 private
 
   def parse name_to_parse
@@ -47,4 +64,29 @@ end
 
 def Card name, suite = nil
   Card[name, suite]
+end
+
+class Deck
+  extend Forwardable
+
+  attr_accessor :cards
+
+  def_delegators :cards, :first, :last, :length
+
+  def initialize
+    self.cards = Deck.cards_for_standard_52_deck
+  end
+
+  def self.cards_for_standard_52_deck
+    cards = []
+
+    Card.suites.each do |suite|
+      Card.names.each do |name|
+        cards << Card[name, suite]
+      end
+    end
+
+    cards
+  end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,1 +1,14 @@
 require File.dirname(__FILE__) + '/spec_helper'
+
+describe Deck do
+
+  it 'should return a standard 52 card deck' do
+    deck = Deck.new
+    deck.length.should == 52
+    deck.first.should  == The2ofSpades
+    deck.last.should   == AceofDiamonds
+  end
+
+  it 'should be able to shuffle'
+
+end
